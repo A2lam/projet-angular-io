00f1eae46306d413c2c5781149efbe39ee79041c
robin <robin.sgeorges@gmail.com>
2020-05-13 15:56:54 +0200

    pdv update

--- src/app/battle/battle.service.spec.ts
+++ src/app/battle/battle.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
 import { BattleService } from './battle.service';
+import {Pokemon} from '../pokemon/pokemon';
 
 describe('BattleService', () => {
   let service: BattleService;
@@ -12,5 +12,24 @@ describe('BattleService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should return pokemon1 when pokemen1.speed > pokemon2.speed', () => {
+    const pokemon1: Pokemon = {
+      name: 'Pikachu',
+      life: 3,
+      speed: 5,
+      isAlive: true,
+      color: 'd8db2e'
+    };
+    const pokemon2: Pokemon = {
+      name: 'Bulbizarre',
+      life: 3,
+      speed: 4,
+      isAlive: true,
+      color: '1aac6c'
+
+    };
+    expect(service.attackFirst(pokemon1, pokemon2)).toBe(pokemon1);
   });
 });
